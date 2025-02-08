a0550320b7e58339a9dc99e3e9d94addc0418bc2
Suronjit797 <suronjit797@gmail.com>
2025-02-08 12:11:31 +0600

    gql for render

--- src/app/graphql.ts
+++ src/app/graphql.ts
@@ -18,9 +18,9 @@ export const resolvers = mergeResolvers([globalResolvers, userResolvers, project
 
 const plugins: any = [];
 
-// if (config.NODE_ENV === "production") {
-//   plugins.push(ApolloServerPluginLandingPageDisabled());
-// }
+if (config.NODE_ENV === "production") {
+  plugins.push(ApolloServerPluginLandingPageDisabled());
+}
 
 // Create the ApolloServer instance with the custom context
 export const graphqlServer = new ApolloServer<GraphqlContext>({
