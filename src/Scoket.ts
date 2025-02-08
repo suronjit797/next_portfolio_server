6b37a487781889248c40f14b91fa6fbc5f9ab22b
Suronjit797 <suronjit797@gmail.com>
2025-02-08 11:39:23 +0600

    clearn up

--- src/Scoket.ts
+++ src/Scoket.ts
@@ -1,70 +1,70 @@
-import { DefaultEventsMap, Server } from "socket.io";
-import { server } from "./server";
-import { decodeToken } from "./app/middleware/auth";
-import UserModel from "./app/modules/user/user.model";
-import ActiveUserModel from "./app/modules/activeUser/activeUser.model";
-import { GET_ACTIVE_USERS, MESSAGE_INPUT_FOCUSED, MESSAGE_TYPING } from "./constants/constantsVars";
+// import { DefaultEventsMap, Server } from "socket.io";
+// import { server } from "./server";
+// import { decodeToken } from "./app/middleware/auth";
+// import UserModel from "./app/modules/user/user.model";
+// import ActiveUserModel from "./app/modules/activeUser/activeUser.model";
+// import { GET_ACTIVE_USERS, MESSAGE_INPUT_FOCUSED, MESSAGE_TYPING } from "./constants/constantsVars";
 
-export const activeUsers: { [key: string]: string } = {};
+// export const activeUsers: { [key: string]: string } = {};
 
-export let io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>;
+// export let io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>;
 
-const initSocket = async () => {
-  // Socket.IO Configuration
-  try {
-    io = new Server(server, {
-      connectionStateRecovery: {},
-      cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-      },
-    });
-    io.on("connection", async (socket) => {
-      const token = socket.handshake.auth.token;
-      const user = await decodeToken(token);
+// const initSocket = async () => {
+//   // Socket.IO Configuration
+//   try {
+//     io = new Server(server, {
+//       connectionStateRecovery: {},
+//       cors: {
+//         origin: "*",
+//         methods: ["GET", "POST"],
+//       },
+//     });
+//     io.on("connection", async (socket) => {
+//       const token = socket.handshake.auth.token;
+//       const user = await decodeToken(token);
 
-      if (!token || !user || !user?._id) {
-        console.log("Unauthorized connection attempt");
-        socket.disconnect();
-        return;
-      }
+//       if (!token || !user || !user?._id) {
+//         console.log("Unauthorized connection attempt");
+//         socket.disconnect();
+//         return;
+//       }
 
-      // connected user
-      console.log(`New client connected`, socket.id, user.name);
-      // activeUsers[user._id] = socket.id;
-      const a = await ActiveUserModel.updateMany(
-        { userId: user._id },
-        { userId: user._id, socketId: socket.id },
-        { new: true, upsert: true }
-      );
+//       // connected user
+//       console.log(`New client connected`, socket.id, user.name);
+//       // activeUsers[user._id] = socket.id;
+//       const a = await ActiveUserModel.updateMany(
+//         { userId: user._id },
+//         { userId: user._id, socketId: socket.id },
+//         { new: true, upsert: true }
+//       );
 
-      await UserModel.findByIdAndUpdate(user._id, { isActive: true }, { new: true });
+//       await UserModel.findByIdAndUpdate(user._id, { isActive: true }, { new: true });
 
-      io.emit(GET_ACTIVE_USERS, { user });
+//       io.emit(GET_ACTIVE_USERS, { user });
 
-      // typing...
-      socket.on(MESSAGE_INPUT_FOCUSED, async ({ status, userId }) => {
-        if (userId) {
-          console.log(userId);
-          const receiverUsers = await ActiveUserModel.find({ userId }).select({ socketId: 1, _id: 0 });
-          const receiverIds = receiverUsers.map((r) => r.socketId);
-          io.to(receiverIds).emit(MESSAGE_TYPING, { typing: Boolean(status) });
-        }
-      });
+//       // typing...
+//       socket.on(MESSAGE_INPUT_FOCUSED, async ({ status, userId }) => {
+//         if (userId) {
+//           console.log(userId);
+//           const receiverUsers = await ActiveUserModel.find({ userId }).select({ socketId: 1, _id: 0 });
+//           const receiverIds = receiverUsers.map((r) => r.socketId);
+//           io.to(receiverIds).emit(MESSAGE_TYPING, { typing: Boolean(status) });
+//         }
+//       });
 
-      // disconnect
-      socket.on("disconnect", async () => {
-        console.log("User disconnected:", socket.id, user.name);
-        // user?._id && delete activeUsers[user._id];
-        const a = await ActiveUserModel.deleteMany({ userId: user._id, socketId: socket.id });
+//       // disconnect
+//       socket.on("disconnect", async () => {
+//         console.log("User disconnected:", socket.id, user.name);
+//         // user?._id && delete activeUsers[user._id];
+//         const a = await ActiveUserModel.deleteMany({ userId: user._id, socketId: socket.id });
 
-        await UserModel.findByIdAndUpdate(user._id, { isActive: false, lastActive: Date.now() }, { new: true });
-        io.emit(GET_ACTIVE_USERS, { user });
-      });
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
+//         await UserModel.findByIdAndUpdate(user._id, { isActive: false, lastActive: Date.now() }, { new: true });
+//         io.emit(GET_ACTIVE_USERS, { user });
+//       });
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-export default initSocket;
+// export default initSocket;
--- src/app/modules/images/_images.graphql.ts
+++ src/app/modules/images/_images.graphql.ts
@@ -1,148 +1,148 @@
-import gql from "graphql-tag";
-import SkillsModel from "./skills.model";
-import skillService from "./skills.service";
-import { GetAllSkills, SkillsPaginationArgs, ISkills } from "./skills.interface";
-import { apolloAuth } from "../../middleware/auth";
-import { paginationHelper } from "../../../helper/paginationHelper";
-import filterHelper from "../../../helper/filterHelper";
-import { GraphqlContext, IPagination } from "../../../shared/globalInterfaces";
-import { userRole } from "../../../constants/userConstants";
-import ApiError from "../../../ApiError";
-import httpStatus from "http-status";
+// import gql from "graphql-tag";
+// import SkillsModel from "./skills.model";
+// import skillService from "./skills.service";
+// import { GetAllSkills, SkillsPaginationArgs, ISkills } from "./skills.interface";
+// import { apolloAuth } from "../../middleware/auth";
+// import { paginationHelper } from "../../../helper/paginationHelper";
+// import filterHelper from "../../../helper/filterHelper";
+// import { GraphqlContext, IPagination } from "../../../shared/globalInterfaces";
+// import { userRole } from "../../../constants/userConstants";
+// import ApiError from "../../../ApiError";
+// import httpStatus from "http-status";
 
-const { superAdmin, admin } = userRole;
+// const { superAdmin, admin } = userRole;
 
-export const skillsTypeDefs = gql`
-  # queries
+// export const skillsTypeDefs = gql`
+//   # queries
 
-  type Skills {
-    _id: ID!
-    name: String!
-    image: ImageType!
-    types: String!
-    createdAt: Date!
-    updatedAt: Date!
-  }
+//   type Skills {
+//     _id: ID!
+//     name: String!
+//     image: ImageType!
+//     types: String!
+//     createdAt: Date!
+//     updatedAt: Date!
+//   }
 
-  # inputs
-  input CreateSkillsInput {
-    name: String!
-    image: ImageInput!
-    types: String!
-  }
+//   # inputs
+//   input CreateSkillsInput {
+//     name: String!
+//     image: ImageInput!
+//     types: String!
+//   }
 
-  input UpdateSkillsInput {
-    name: String
-    image: ImageInput
-    types: String
-  }
+//   input UpdateSkillsInput {
+//     name: String
+//     image: ImageInput
+//     types: String
+//   }
 
-  input SkillsQuery {
-    name: String
-    types: String
-    createdAt: Date
-    updatedAt: Date
-    search: String
-  }
+//   input SkillsQuery {
+//     name: String
+//     types: String
+//     createdAt: Date
+//     updatedAt: Date
+//     search: String
+//   }
 
-  type getAllSkillsQuery {
-    meta: MetaQuery
-    data: [Skills!]
-  }
+//   type getAllSkillsQuery {
+//     meta: MetaQuery
+//     data: [Skills!]
+//   }
 
-  # query
-  type Query {
-    skills(pagination: PaginationInput, query: SkillsQuery): getAllSkillsQuery!
-    skill(id: ID!): Skills
-    # getUserSkills(id: ID!, pagination: PaginationInput): getAllSkillsQuery
-    # profile: Skills
-  }
+//   # query
+//   type Query {
+//     skills(pagination: PaginationInput, query: SkillsQuery): getAllSkillsQuery!
+//     skill(id: ID!): Skills
+//     # getUserSkills(id: ID!, pagination: PaginationInput): getAllSkillsQuery
+//     # profile: Skills
+//   }
 
-  # mutation
-  type Mutation {
-    createSkills(body: CreateSkillsInput!): Skills!
-    updateSkill(id: ID!, body: UpdateSkillsInput): Skills!
-    deleteSkill(id: ID!): Skills!
-    deleteManySkills(query: SkillsQuery!): Skills!
-  }
-`;
+//   # mutation
+//   type Mutation {
+//     createSkills(body: CreateSkillsInput!): Skills!
+//     updateSkill(id: ID!, body: UpdateSkillsInput): Skills!
+//     deleteSkill(id: ID!): Skills!
+//     deleteManySkills(query: SkillsQuery!): Skills!
+//   }
+// `;
 
-export const skillsResolvers = {
-  Query: {
-    skills: async (_: undefined, args: SkillsPaginationArgs, context: GraphqlContext): Promise<GetAllSkills> => {
-      const { req } = context;
-      // Authorization
-      await apolloAuth(req);
+// export const skillsResolvers = {
+//   Query: {
+//     skills: async (_: undefined, args: SkillsPaginationArgs, context: GraphqlContext): Promise<GetAllSkills> => {
+//       const { req } = context;
+//       // Authorization
+//       await apolloAuth(req);
 
-      // Pagination and filtering
-      const page = paginationHelper(args.pagination);
-      const filter = filterHelper(args.query || {}, new SkillsModel(), ["name", "email"]);
+//       // Pagination and filtering
+//       const page = paginationHelper(args.pagination);
+//       const filter = filterHelper(args.query || {}, new SkillsModel(), ["name", "email"]);
 
-      // Fetch skills
-      return await skillService.getAll(page, filter);
-    },
-    skill: async (_: undefined, args: { id: string }, context: GraphqlContext): Promise<ISkills> => {
-      const { req } = context;
-      // Authorization
-      await apolloAuth(req);
+//       // Fetch skills
+//       return await skillService.getAll(page, filter);
+//     },
+//     skill: async (_: undefined, args: { id: string }, context: GraphqlContext): Promise<ISkills> => {
+//       const { req } = context;
+//       // Authorization
+//       await apolloAuth(req);
 
-      // Fetch skills
-      return await skillService.getSingle(args.id);
-    },
-  },
+//       // Fetch skills
+//       return await skillService.getSingle(args.id);
+//     },
+//   },
 
-  Mutation: {
-    createSkills: async (
-      _: undefined,
-      args: { body: Partial<ISkills> },
-      context: GraphqlContext
-    ): Promise<ISkills | null> => {
-      // Authorization
-      await apolloAuth(context.req);
+//   Mutation: {
+//     createSkills: async (
+//       _: undefined,
+//       args: { body: Partial<ISkills> },
+//       context: GraphqlContext
+//     ): Promise<ISkills | null> => {
+//       // Authorization
+//       await apolloAuth(context.req);
 
-      //
-      const skills = await skillService.create(args.body);
-      return skills;
-    },
+//       //
+//       const skills = await skillService.create(args.body);
+//       return skills;
+//     },
 
-    updateSkill: async (
-      _: undefined,
-      args: { id: string; body: Partial<ISkills> },
-      { req }: GraphqlContext
-    ): Promise<ISkills | null> => {
-      // Authorization
-      await apolloAuth(req);
-      // Update project
-      return await skillService.update(args.id, args.body);
-    },
+//     updateSkill: async (
+//       _: undefined,
+//       args: { id: string; body: Partial<ISkills> },
+//       { req }: GraphqlContext
+//     ): Promise<ISkills | null> => {
+//       // Authorization
+//       await apolloAuth(req);
+//       // Update project
+//       return await skillService.update(args.id, args.body);
+//     },
 
-    deleteSkill: async (_: undefined, args: { id: string }, context: GraphqlContext): Promise<ISkills> => {
-      // Authorization
-      await apolloAuth(context.req, superAdmin, admin);
+//     deleteSkill: async (_: undefined, args: { id: string }, context: GraphqlContext): Promise<ISkills> => {
+//       // Authorization
+//       await apolloAuth(context.req, superAdmin, admin);
 
-      // Delete project
-      return await skillService.remove(args.id);
-    },
+//       // Delete project
+//       return await skillService.remove(args.id);
+//     },
 
-    deleteManySkills: async (
-      _: undefined,
-      args: { filter: Partial<ISkills> },
-      context: GraphqlContext
-    ): Promise<ISkills> => {
-      const filter = filterHelper(args.filter || {}, new SkillsModel(), [
-        "name",
-        "description",
-        "packages",
-        "tags",
-        "liveUrl",
-      ]);
-      // Authorization
-      const user = await apolloAuth(context.req, superAdmin, admin);
-      if (user.role !== superAdmin && !Object.keys(filter).length) {
-        throw new ApiError(httpStatus.NOT_ACCEPTABLE, "Filter is empty");
-      }
-      // Delete project
-      return await skillService.removeMany(filter);
-    },
-  },
-};
+//     deleteManySkills: async (
+//       _: undefined,
+//       args: { filter: Partial<ISkills> },
+//       context: GraphqlContext
+//     ): Promise<ISkills> => {
+//       const filter = filterHelper(args.filter || {}, new SkillsModel(), [
+//         "name",
+//         "description",
+//         "packages",
+//         "tags",
+//         "liveUrl",
+//       ]);
+//       // Authorization
+//       const user = await apolloAuth(context.req, superAdmin, admin);
+//       if (user.role !== superAdmin && !Object.keys(filter).length) {
+//         throw new ApiError(httpStatus.NOT_ACCEPTABLE, "Filter is empty");
+//       }
+//       // Delete project
+//       return await skillService.removeMany(filter);
+//     },
+//   },
+// };
